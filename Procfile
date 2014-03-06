d26e0adbd27c1822610917122063193face9500a
nidhinp <nidhinp.412@gmail.com>
2014-03-06 23:32:54 +0530

    deploying on heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn graphs_django.wsgi
--- graphs_django/settings.py
+++ graphs_django/settings.py
@@ -111,3 +111,14 @@ TEMPLATE_CONTEXT_PROCESSORS = (
 TEMPLATE_DIRS = (
         map_path('templates'),
 )
+
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
